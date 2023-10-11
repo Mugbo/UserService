96337f7edf87952c1d10b51136ae90f73151f461
Michael Agbo <Michael.Agbo@genesys.com>
2023-10-11 09:59:05 +0100

    Created RegistrationServiceClient interface UserDetails class UserServiceApplication class

---
+++ src/main/java/ie/atu/userservice/RegistrationServiceClient.java
@@ -0,0 +1,12 @@
+package ie.atu.userservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
+public interface RegistrationServiceClient {
+    @PostMapping("/confirm")
+    String someDetails(@RequestBody UserDetails userDetails);
+}
